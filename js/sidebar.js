954ccc7a0e8bb736007e164640819800ae4ff8c0
Tayef_Shah <tayefshah@gmail.com>
2018-09-23 10:31:23 -0400

    Script edit

--- js/sidebar.js
+++ js/sidebar.js
@@ -1,7 +1,26 @@
+var name;
+var weight;
+var price;
+
 $(document).ready(function() {
-    console.log( "ready!");    
+    console.log( "ready!");
+    console.log(JSON.stringify());
 });
 
 $.getJSON('../product.json', function(json) {
+    console.log("JSON file");
     console.log(json); // this will show the info it in firebug console
+    console.log(json.list[0].product_name);
+
+    console.log("List of items");
+    for(var i = 0; i < json.list.length; i++){
+
+        name = json.list[i].product_name;
+        weight = json.list[i].net_weight;
+        price = json.list[i].shelf_width;
+
+        console.log(name);
+        console.log(weight);
+        console.log(price);
+    }
 });
