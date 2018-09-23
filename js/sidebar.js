b83ae1b77fba20e5bd41c4dc6c0b12620ef56897
Veraj <veraj.paruthi@gmail.com>
2018-09-23 10:20:38 -0400

    sidebar.js

--- js/sidebar.js
+++ js/sidebar.js
@@ -1,26 +1,7 @@
-var name;
-var weight;
-var price;
-
 $(document).ready(function() {
-    console.log( "ready!");
-    console.log(JSON.stringify());
+    console.log( "ready!");    
 });
 
 $.getJSON('../product.json', function(json) {
-    console.log("JSON file");
     console.log(json); // this will show the info it in firebug console
-    console.log(json.list[0].product_name);
-
-    console.log("List of items");
-    for(var i = 0; i < json.list.length; i++){
-
-        name = json.list[i].product_name;
-        weight = json.list[i].net_weight;
-        price = json.list[i].shelf_width;
-
-        console.log(name);
-        console.log(weight);
-        console.log(price);
-    }
 });
